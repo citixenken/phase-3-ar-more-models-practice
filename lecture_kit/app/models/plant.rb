026875acb09ae519878deed0051b943ffae5d2d9
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-27 20:04:48 +0300

    COMPLETED: ar relationships - more models

--- lecture_kit/app/models/plant.rb
+++ lecture_kit/app/models/plant.rb
@@ -1,5 +1,22 @@
 class Plant < ActiveRecord::Base
+    has_many :plant_categories
+    has_many :categories, through: :plant_categories
+
     has_many :plant_parenthoods
-    has_many :plant_parents, through: :plant_parenthoods
-    belongs_to :category
+    has_many :people, through: :plant_parenthoods
+
+    # has_many :waterings
+    # has_many :people, through: :waterings
+
+    has_many :waterings
+    has_many :waterers, through: :waterings
+
+    def number_of_days_since_the_last_watering
+        today = DateTime.now
+        last_watering = self.waterings.last.created_at.to_datetime
+        number_of_days = (today - last_watering).to_i
+
+        return puts "I was watered #{number_of_days} days ago" if number_of_days > 0
+        puts "I was watered today!"
+    end
 end
--- lecture_kit/app/models/plant_parenthood.rb
+++ lecture_kit/app/models/plant_parenthood.rb
@@ -1,13 +1,11 @@
 class PlantParenthood < ActiveRecord::Base
 
-     belongs_to :plant_parent
-     belongs_to :plant
+    before_save :cap_affection
 
-    # def plant_parents
-    #     # before ActiveRecord
-    #     PlantParent.all.select{|plant_parent| plant_parent == self.plant_parents}
-    #     # AR:
-    #     id_num = self.plant_parent_id
-    #     PlantParent.find(id_num)
-    # end
+    belongs_to :person
+    belongs_to :plant
+
+    def cap_affection
+        self.affection = 11_000 if self.affection > 11_000
+    end
 end
